acb26ab6c601d39645f9dd535a4ab948aa8eebf9
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2020-02-01 18:54:27 +0100

    added prefabs

---
+++ Assets/Scripts/GameEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine : MonoBehaviour
+{
+    //static GameEngine Instance = new GameEngine();
+    public GameObject RootObject;
+    public GameObject DnaChain;
+    public GameObject DnaPair;
+
+    void Start()
+    {
+        InitChain();
+    }
+
+    private void InitChain()
+    {
+        var rootObject = Instantiate(RootObject);
+        var dnaPair = InstantiateDnaPair();
+        dnaPair.transform.parent = rootObject.transform;
+
+    }
+
+    private GameObject InstantiateDnaPair()
+    {
+        var dnaPair = Instantiate(DnaPair);
+     var childrenComponents   =dnaPair.GetComponentsInChildren<Material>();
+
+    }
+}
