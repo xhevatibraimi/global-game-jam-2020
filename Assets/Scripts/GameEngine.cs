3c53c0c7c8ac9e0a973d578a412dfa19374582d1
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2020-02-01 19:35:55 +0100

    added dna chain pairs generation

--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -18,16 +18,29 @@ public class GameEngine : MonoBehaviour
     private void InitChain()
     {
         var rootObject = Instantiate(RootObject);
-        var dnaPair = InstantiateDnaPair();
-        dnaPair.transform.parent = rootObject.transform;
+        var dnaChainObject = Instantiate(DnaChain);
+        dnaChainObject.transform.parent = rootObject.transform;
 
+        var counter = 0;
+        var rotationY = 0.0f;
+        var positionY = 0.0f;
+        while (counter < 5)
+        {
+            var dnaPair = InstantiateDnaPair();
+            dnaPair.transform.localPosition = new Vector3(0, positionY, 0);
+            dnaPair.transform.localRotation = new Quaternion(0, rotationY, 0, 0);
+            dnaPair.transform.parent = dnaChainObject.transform;
+            counter++;
+            rotationY += 90.0f;
+            positionY += 2.5f;
+        }
     }
 
     private GameObject InstantiateDnaPair()
     {
         var dnaPair = Instantiate(DnaPair);
-        var childrenComponents = dnaPair.GetComponentsInChildren<Material>();
-        
+        //var childrenComponents = dnaPair.GetComponentsInChildren<Material>();
+
         // modify materials
 
         return dnaPair;
