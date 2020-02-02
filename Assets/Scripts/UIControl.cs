b79da33f69afc8874da8556988b46b1cd3d1fe95
Darko Kotoski <wbountyhunter@hotmail.com>
2020-02-02 16:47:26 +0100

    Added UI prefab

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIControl : MonoBehaviour
+{
+    public GameObject StartButton;
+
+    public GameObject Score;
+
+    public GameObject GameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        StartButton.SetActive(false);
+        Score.SetActive(true);
+        GameOver.SetActive(false);
+        // game start here
+    }
+
+    public void UpdateScore(int score)
+    {
+        Score.GetComponent<TextElement>().text = score.ToString();
+    }
+
+    public void EndGame(){
+        Score.SetActive(false);
+        GameOver.SetActive(true);
+        GameOver.GetComponent<TextElement>().text = "Game over /n Score: " + Score.GetComponent<TextElement>().text;
+    }
+}
