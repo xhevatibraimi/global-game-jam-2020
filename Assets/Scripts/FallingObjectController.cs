baf2039d7cddcff3d4148921f1863e576bfa7593
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2020-02-02 02:05:35 +0100

    added color generation

--- Assets/Scripts/FallingObjectController.cs
+++ Assets/Scripts/FallingObjectController.cs
@@ -5,8 +5,8 @@ public class FallingObjectController : MonoBehaviour
 
     public float FallingSpeed = 1.0f;
 
-    public const float YSpeed = 2.0f;
-    public const float RotatingSpeed = 15.0f;
+    public float YSpeed = 2.0f;
+    public float RotatingSpeed = 15.0f;
     private Rigidbody rb;
     private Transform trans;
 
--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -10,19 +10,40 @@ public class GameEngine : MonoBehaviour
     public GameObject RootObject;
     public GameObject DnaChain;
     public GameObject DnaPair;
+    public Material RedMaterial;
+    public Material GreenMaterial;
+    public Material BlueMaterial;
+    public Material YellowMaterial;
+    public Material FrameMaterial;
     public int NumberOfDnaPairs = 1;
+    public float RotatingSpeed;
+    public float YSpeed;
+    public float DnaChainInitialPosition;
+
+
+    private List<Material> Materials = new List<Material>();
+    private Material previousMaterial = null;
+    private System.Random random = new System.Random();
     void Start()
     {
         InitChain();
     }
 
+
     private void InitChain()
     {
+        Materials.Add(RedMaterial);
+        Materials.Add(GreenMaterial);
+        Materials.Add(BlueMaterial);
+        Materials.Add(YellowMaterial);
+        var randomNumber = random.Next(0, 4);
+        previousMaterial = Materials[randomNumber];
+
         var rootObject = Instantiate(RootObject);
         var fallingObject = Instantiate(FallingObject);
         fallingObject.transform.parent = rootObject.transform;
-        var dnaChainObject = Instantiate(DnaChain);
-        dnaChainObject.transform.parent = fallingObject.transform;
+        var dnaChainObject = Instantiate(DnaChain, new Vector3(0, DnaChainInitialPosition, 0), new Quaternion(), fallingObject.transform);
+        //dnaChainObject.transform.parent = fallingObject.transform;
 
         var counter = 0;
         var rotationY = 0.0f;
@@ -33,18 +54,44 @@ public class GameEngine : MonoBehaviour
             dnaPair.transform.localPosition = new Vector3(0, positionY, 0);
             dnaPair.transform.localRotation = Quaternion.Euler(0, rotationY, 0);
             dnaPair.transform.parent = dnaChainObject.transform;
+
+
+            var childRenderers = dnaPair.GetComponentsInChildren<Renderer>();
+
+            foreach (var renderer in childRenderers)
+            {
+                if (renderer.gameObject.CompareTag("Bridge"))
+                {
+                    renderer.material = GetRandomMaterial();
+                }
+                else
+                {
+                    renderer.material = FrameMaterial;
+                }
+            }
+
             counter++;
-            rotationY -= FallingObjectController.RotatingSpeed;
-            positionY += FallingObjectController.YSpeed;
+            rotationY -= RotatingSpeed;
+            positionY += YSpeed;
         }
+    }
+
+    private Material GetRandomMaterial()
+    {
+        Material material = null;
+        do
+        {
+            var randomNumber = random.Next(0, 4);
+            material = Materials[randomNumber];
+        } while (material == previousMaterial);
+        previousMaterial = material;
+        return material;
     }
 
     private GameObject InstantiateDnaPair()
     {
         var dnaPair = Instantiate(DnaPair);
-        //var childrenComponents = dnaPair.GetComponentsInChildren<Material>();
 
-        // modify materials
 
         return dnaPair;
     }
